7147e4e7f35f4bfa7e0a07225be3176e690d258d
Lawkmonster <148557062+Lawkmonster@users.noreply.github.com>
2026-02-08 20:50:17 +0800

    Add Node2D scene and scaffold logic script

---
+++ logic.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var character_rarity: Dictionary[String, int] = {
+"common": 0.6,
+"rare": 0.3,
+"legendary": 0.1  
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func pullLOGIC():
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
