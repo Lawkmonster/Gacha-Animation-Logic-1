313ffda084c69f26649d9e53c85e674514807812
Lawkmonster <148557062+Lawkmonster@users.noreply.github.com>
2026-02-09 18:27:13 +0800

    added some base code

--- logic.gd
+++ logic.gd
@@ -1,15 +1,21 @@
 extends Node
 
-var character_rarity: Dictionary[String, int] = {
+var character_rarity: Dictionary[String, float] = {
 "common": 0.6,
 "rare": 0.3,
 "legendary": 0.1  
 }
 
+func _ready():
+	print("=== Gacha Pull Logic Ready ===")
+	print("Press SPACE to pull")
+	print("Rarities: Common 60%, Rare 30%, Legendary 10%")
+	print("Pity: Every 10 pulls guaranteed Legendary\n")
 
-# Called when the node enters the scene tree for the first time.
 func pullLOGIC():
-	
+	pass
 
 
+func probCALC():
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
